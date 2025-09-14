ea6403f090967db34cf48040a8059954570e30f2
Vadim <vadimhus@MacBook-Air-Vadim.local>
2025-09-14 23:57:46 +0300

    order: defined OrderStatus enum at status.py with docs

--- src/pizza/domain/status.py
+++ src/pizza/domain/status.py
@@ -1,1 +1,26 @@
-"""Заглушка: перечисление статусов заказа и описание допустимых переходов."""
+from enum import Enum
+
+
+class OrderStatus(Enum):
+    """
+    Order status and available passing
+    NEW -> ACCEPTED -> BAKING -> BOXED -> DISPATCHED -> DELIVERED
+    NEW -> CANCELED
+    ACCEPTED -> CANCELED
+    No cancellation after BAKING.
+    """
+
+    NEW = "new"
+    """Order created, but not accepted in the restaurant."""
+    ACCEPTED = "accepted"
+    """Order accepted, but not baking."""
+    BAKING = "baking"
+    """Order is baking."""
+    BOXED = "boxed"
+    """Order baked and boxed."""
+    DISPATCHED = "dispatched"
+    """Courier received order."""
+    DELIVERED = "delivered"
+    """Order delivered."""
+    CANCELED = "canceled"
+    """Order canceled."""
